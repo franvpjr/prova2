6b609c5987f51c198b24f3c5f3c7bc0595a1c9ac
franvpjr <franvpjr@hotmail.com>
2024-11-11 11:31:55 -0300

    Add files via upload

---
+++ prova2.py
@@ -0,0 +1,7 @@
+dicionario = {}
+
+dicionario['nome'] = input('digite seu nome')
+dicionario['telefone'] = input('digite seu telefone')
+dicionario['email'] = input('digite seu email')
+
+print(dicionario)
